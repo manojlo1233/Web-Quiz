e7b2d4e97cccb18ac6e697ecba4c18593595c956
manojlo1233 <milan.manojlo00@gmail.com>
2025-05-14 17:20:18 +0200

    Implementing registration page

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  firstName: string = '';
+  secondName: string = '';
+  email: string = '';
+  username: string = '';
+  password: string = '';
+  country: string = '';
+  birth: string = '';
 
+  benefits: string[] = [
+    "Interactive learning experiences that help you explore new topics.",
+    "Knowledge-based challenges and competitions to test your skills.",
+    "Fun mini-games and animations that make learning engaging and enjoyable.",
+    "A community of curious minds, just like you."
+  ]
+
+  onSubmit() {
+    console.log('Logging in with', this.email, this.password);
+  }
 }
